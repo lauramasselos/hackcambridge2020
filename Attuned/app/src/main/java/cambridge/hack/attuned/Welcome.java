a5fa14bc08ca5d79622cd9db5c62f91e94e7c0db
lauramasselos <s1603459@sms.ed.ac.uk>
2020-01-18 17:04:51 +0000

    imports

--- Attuned/app/src/main/java/cambridge/hack/attuned/Welcome.java
+++ Attuned/app/src/main/java/cambridge/hack/attuned/Welcome.java
@@ -2,8 +2,18 @@ package cambridge.hack.attuned;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 public class Welcome extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "edd58336b453498387f510f105286d74";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
