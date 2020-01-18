4a0fc46b46584ee0651c37f772a27aae7e9b4ca8
lauramasselos <s1603459@sms.ed.ac.uk>
2020-01-18 23:00:45 +0000

    initial login screen

---
+++ Attuned/app/src/main/java/cambridge/hack/attuned/LoginActivity.java
@@ -0,0 +1,104 @@
+package cambridge.hack.attuned;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
+    private static final int REQUEST_CODE = 1337;
+    private static final String CLIENT_ID = "your_client_id";
+    private static final String REDIRECT_URI = "http://google.com";
+    private SpotifyAppRemote mSpotifyAppRemote;
+    private static final String SCOPES = "user-read-recently-played,user-library-modify,user-read-email,user-read-private";
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+
+    private RequestQueue queue;
+
+
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{SCOPES});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    editor.apply();
+                    //waitForUserInfo();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide();
+        setContentView(R.layout.activity_welcome);
+
+
+        authenticateSpotify();
+
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
+    }
+
+
+}
--- Attuned/app/src/main/java/cambridge/hack/attuned/Welcome.java
+++ Attuned/app/src/main/java/cambridge/hack/attuned/Welcome.java
@@ -1,6 +1,7 @@
 package cambridge.hack.attuned;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,18 +14,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
-import com.spotify.android.appremote.api.SpotifyAppRemote;
 
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
+public class Welcome extends AppCompatActivity {
 
-public class Welcome extends AppCompatActivity implements Spotifylogin.OnFragmentInteractionListener {
-
-    private static final String CLIENT_ID = "edd58336b453498387f510f105286d74";
-    private SpotifyAppRemote mSpotifyAppRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +27,15 @@ public class Welcome extends AppCompatActivity implements Spotifylogin.OnFragmen
         Button btn;
         btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                addFrag();
+                // go to LoginActivity
+                Intent myIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(myIntent);
             }
         });
 
-
-    }
-    private void addFrag(){
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        Spotifylogin spotlog = new Spotifylogin();
-        ft.add(R.id.frc, spotlog);
-        ft.commit();
     }
 
-
-    @Override
-    public void onFragmentInteraction(Uri uri){
-        //you can leave it empty
-    }
 
 
 
