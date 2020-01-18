466e733e8e45a73b6c19fbea1a6bfe2f32a15680
Anna <s1601847@sms.ed.ac.uk>
2020-01-18 20:03:54 +0000

    i made the fragment work

--- Attuned/app/src/main/java/cambridge/hack/attuned/Welcome.java
+++ Attuned/app/src/main/java/cambridge/hack/attuned/Welcome.java
@@ -1,7 +1,18 @@
 package cambridge.hack.attuned;
 
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -10,7 +21,7 @@ import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
-public class Welcome extends AppCompatActivity {
+public class Welcome extends AppCompatActivity implements Spotifylogin.OnFragmentInteractionListener {
 
     private static final String CLIENT_ID = "edd58336b453498387f510f105286d74";
     private SpotifyAppRemote mSpotifyAppRemote;
@@ -19,5 +30,33 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+        Button btn;
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFrag();
+            }
+        });
+
+
     }
+    private void addFrag(){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Spotifylogin spotlog = new Spotifylogin();
+        ft.add(R.id.frc, spotlog);
+        ft.commit();
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
+    }
+
+
+
 }
