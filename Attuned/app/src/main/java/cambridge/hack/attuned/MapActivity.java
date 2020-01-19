05c2a52192a1eacf65aa7b9b8cfd5e0ebd12d2c5
Anna <s1601847@sms.ed.ac.uk>
2020-01-19 01:57:07 +0000

    got mapbox workking

--- Attuned/app/src/main/java/cambridge/hack/attuned/MapActivity.java
+++ Attuned/app/src/main/java/cambridge/hack/attuned/MapActivity.java
@@ -1,34 +1,67 @@
 package cambridge.hack.attuned;
 
+import android.location.Location;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
 
-public class MapActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity implements PermissionsListener, LocationEngineListener {
 
     private MapView mapView;
     private MapboxMap map;
     private PermissionsManager permissionsManager;
     private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance( this, "pk.eyJ1IjoicnR1cnBpZSIsImEiOiJjazVqZmpzN20wMm4wM2txYzBwbTZtdGdjIn0.JG3O13–7SSLHi3qPf69CkQ");
+        Mapbox.getInstance( this, "pk.eyJ1IjoiczE2MDE4NDciLCJhIjoiY2puMXBoeXplMnNicTNxbzhhYWFmbnhqZyJ9.hTS5UNqpWkpg2Fcy4z4fAQ");
         setContentView(R.layout.activity_map);
+        Log.d("boofdaddy","mapbox load map");
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this::onMapReady);
 
     }
-    
+
+
+    public void onMapReady(MapboxMap mapboxMap) {
+        if (mapboxMap == null) {
+            Log.d("boof", "[onMapReady] mapBox is null");
+        } else {
+            map = mapboxMap;
+            Log.d("boofdaddy","mapbox enable loction?");
+            // Set user interface options
+            // Make location information available
+            enableLocation();
+            map.getUiSettings().setCompassEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            //this sets the onMarkerclick method as the one at the bottom
+            //map.setOnMarkerClickListener(this::onMarkerClick);
+        }
+    }
 
     @Override
     protected void onStart() {
@@ -70,5 +103,92 @@ public class MapActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+    //mapbox additional methods
+    //initializes a location engine, sets how fast/slow it updates location
+    // sets camera position of the map to a specific location
+    private void setCameraPosition(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(),
+                location.getLongitude());
+        map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationLayer() {
+        if (mapView == null) {
+            Log.d("boof", "mapView is null");
+        } else {
+            if (map == null) {
+                Log.d("boof", "map is null");
+            } else {
+                Log.d("boof", "[ERROR TESTING} SHOULD SHOW UP IF INITALIZING NEW POSITION and map is not null" );
+                locationLayerPlugin = new LocationLayerPlugin(mapView,
+                        map, locationEngine);
+                locationLayerPlugin.setLocationLayerEnabled(true);
+                locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+                locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+            }
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationEngine() {
+        locationEngine = new LocationEngineProvider(this)
+                .obtainBestLocationEngineAvailable();
+        locationEngine.setInterval(5000); // preferably every 5 seconds
+        locationEngine.setFastestInterval(1000); // at most every second
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null) {
+            originLocation = lastLocation;
+            setCameraPosition(lastLocation);
+        } else {
+            Log.d("boof","Lastlocation is null");
+            locationEngine.addLocationEngineListener(this);
+        }
+    }
+
+    private void enableLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            Log.d("boof", "Permissions are granted");
+            initializeLocationEngine();
+            initializeLocationLayer();
+        } else {
+            Log.d("boof", "Permissions are not granted");
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onConnected() {
+
+    }
+
+    //changes camera position based on users location
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            Log.d("boof", "[onLocationChanged] location is null");
+        } else {
+            Log.d("boof", "[onLocationChanged] location is not null");
+            originLocation = location;
+            setCameraPosition(location);
+        }
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
     }
 }
